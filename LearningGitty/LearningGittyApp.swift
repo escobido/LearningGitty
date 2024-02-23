56cc9af80d37c647761e20ead795919f5bfeb1e1
Matthew Escobido <matthew.escobido@gmail.com>
2024-02-23 15:04:28 +0800

    Added first change

---
+++ LearningGitty/LearningGittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGittyApp.swift
+//  LearningGitty
+//
+//  Created by firstblock on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningGittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
